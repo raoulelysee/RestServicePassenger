ec80ed8a584e381b650779d781e5681637082e83
Ray Elysee <raoulelysee1@Rays-MacBook-Pro.local>
2021-12-11 16:41:23 -0500

    commit

---
+++ src/main/java/com/ray/restws/PassengerserviceApplication.java
@@ -0,0 +1,12 @@
+package com.ray.restws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerserviceApplication.class, args);
+	}
+}
